b6f6a6d0f10fb545e43691133fcb5872152ff603
Wilbur Lepcha <wmlepcha15@gmail.com>
2025-11-26 01:42:52 +0530

    solved the overlapping issue

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -14,7 +14,7 @@ export default function MainLayout({ children, title = 'VALENSITA' }: MainLayout
   return (
     <>
       <Head title={title} />
-      <div className="min-h-screen flex flex-col bg-gradient-to-br from-neutral-100 to-neutral-200">
+      <div className="min-h-screen flex flex-col bg-gradient-to-br from-neutral-100 to-neutral-200 overflow-x-hidden">
         <Header />
         <main className="flex-grow">
           {children}
